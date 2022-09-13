631d874fdfd63bb01f7394b6374a1204717e6d3d
hoangvo-hcmut <113313139+hoangvo-hcmut@users.noreply.github.com>
2022-09-13 11:17:09 +0700

    Update main.cpp

--- week2/oop-and-complexity-programming-code/3/main.cpp
+++ week2/oop-and-complexity-programming-code/3/main.cpp
@@ -1,5 +1,3 @@
-#include <cmath>
-#include <string>
 #include <iostream>
 using namespace std;
 
@@ -7,147 +5,119 @@ class BinExp;
 class UnExp;
 class IntLit;
 
-class Visitor
-{
+class Visitor {
 public:
-  virtual string visit(BinExp *exp) = 0;
-  virtual string visit(UnExp *exp) = 0;
-  virtual string visit(IntLit *exp) = 0;
+  virtual int visit(BinExp *exp) = 0;
+  virtual int visit(UnExp *exp) = 0;
+  virtual int visit(IntLit *exp) = 0;
 };
 
-class Exp
-{
+class Exp {
 public:
-  virtual string accept(Visitor &v) = 0;
+  virtual int accept(Visitor *v) = 0;
 };
 
-class BinExp : public Exp
-{
+class BinExp : public Exp {
 public:
   Exp *left;
   char op;
   Exp *right;
-  BinExp(Exp *left, char op, Exp *right)
-  {
+  BinExp(Exp *left, char op, Exp *right) {
     this->left = left;
     this->op = op;
     this->right = right;
   }
 
-  string accept(Visitor &v)
-  {
-    return v.visit(this);
-  };
+  int accept(Visitor *v) { return v->visit(this); };
 };
 
-class UnExp : public Exp
-{
+class UnExp : public Exp {
 public:
   char op;
   Exp *exp;
-  UnExp(char op, Exp *exp)
-  {
+  UnExp(char op, Exp *exp) {
     this->op = op;
     this->exp = exp;
   }
 
-  string accept(Visitor &v)
-  {
-    return v.visit(this);
-  };
+  int accept(Visitor *v) { return v->visit(this); };
 };
 
-class IntLit : public Exp
-{
+class IntLit : public Exp {
 
 public:
   int v;
-  IntLit(int v)
-  {
-    this->v = v;
-  }
+  IntLit(int v) { this->v = v; }
 
-  string accept(Visitor &v)
-  {
-    return v.visit(this);
-  };
+  int accept(Visitor *v) { return v->visit(this); };
 };
 
-class Eval : public Visitor
-{
+class Eval : public Visitor {
 public:
-  string visit(BinExp *exp) override
-  {
-    switch (exp->op)
-    {
+  int visit(BinExp *exp) override {
+    switch (exp->op) {
     case '+':
-      return to_string(stoi(exp->left->accept(*this)) + stoi(exp->right->accept(*this)));
+      return exp->left->accept(this) + exp->right->accept(this);
     case '-':
-      return to_string(stoi(exp->left->accept(*this)) - stoi(exp->right->accept(*this)));
+      return exp->left->accept(this) - exp->right->accept(this);
     case '*':
-      return to_string(stoi(exp->left->accept(*this)) * stoi(exp->right->accept(*this)));
+      return exp->left->accept(this) * exp->right->accept(this);
     case '/':
-      return to_string(stoi(exp->left->accept(*this)) / stoi(exp->right->accept(*this)));
+      return exp->left->accept(this) / exp->right->accept(this);
     };
     return 0;
   }
 
-  string visit(UnExp *exp) override
-  {
+  int visit(UnExp *exp) override {
     if (exp->op == '-')
-      return to_string(-stoi(exp->exp->accept(*this)));
-    return exp->exp->accept(*this);
+      return -exp->exp->accept(this);
+    return exp->exp->accept(this);
   }
 
-  string visit(IntLit *exp) override
-  {
-    return to_string(exp->v);
-  }
+  int visit(IntLit *exp) override { return exp->v; }
 };
 
-class PrintPrefix : public Visitor
-{
+class PrintPrefix : public Visitor {
 public:
-  string visit(BinExp *exp) override
-  {
-    return string(1, exp->op) + string(1, ' ') + exp->left->accept(*this) + string(1, ' ') + exp->right->accept(*this);
+  int visit(BinExp *exp) override {
+    cout << exp->op << " ";
+    exp->left->accept(this);
+    cout << " ";
+    exp->right->accept(this);
+    return 0;
   }
 
-  string visit(UnExp *exp) override
-  {
-    return (exp->op == '+' ? "+. " : "-. ") + exp->exp->accept(*this);
+  int visit(UnExp *exp) override {
+    cout << (exp->op == '+' ? "+. " : "-. ");
+    exp->exp->accept(this);
+    return 0;
   }
 
-  string visit(IntLit *exp) override
-  {
-    return to_string(exp->v);
+  int visit(IntLit *exp) override {
+    cout << to_string(exp->v);
+    return 0;
   }
 };
 
-class PrintPostfix : public Visitor
-{
+class PrintPostfix : public Visitor {
 public:
-  string visit(BinExp *exp) override
-  {
-    return exp->left->accept(*this) + string(1, ' ') + exp->right->accept(*this) + string(1, ' ') + string(1, exp->op);
+  int visit(BinExp *exp) override {
+    exp->left->accept(this);
+    cout << " ";
+    exp->right->accept(this);
+    cout << " ";
+    cout << exp->op;
+    return 0;
   }
 
-  string visit(UnExp *exp) override
-  {
-    return exp->exp->accept(*this) + (exp->op == '+' ? " +." : " -.");
+  int visit(UnExp *exp) override {
+    exp->exp->accept(this);
+    cout << (exp->op == '+' ? " +." : " -.");
+    return 0;
   }
 
-  string visit(IntLit *exp) override
-  {
-    return to_string(exp->v);
+  int visit(IntLit *exp) override {
+    cout << to_string(exp->v);
+    return 0;
   }
 };
-
-int main()
-{
-  BinExp *bii = new BinExp(new IntLit(1), '+', new IntLit(2));
-
-  PrintPrefix printPrefix;
-
-  cout << bii->accept(printPrefix);
-}
