f0b489c4c731aea6503b576b95be84c73c6e68cf
hoangvo-hcmut <113313139+hoangvo-hcmut@users.noreply.github.com>
2022-10-24 03:41:24 +0700

    Create 4.cpp

---
+++ week6/basic-tree-programming-code/4.cpp
@@ -0,0 +1,7 @@
+template <typename E>
+void LNRtraversal(BNode<E> * root, void (*f)(E)) {
+    if (!root) return;
+    LNRtraversal(root->left(), f);
+    f(root->element());
+    LNRtraversal(root->right(), f);
+}
